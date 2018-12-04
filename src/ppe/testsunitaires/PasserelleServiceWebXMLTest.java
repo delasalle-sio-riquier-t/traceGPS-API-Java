4804d5ce502ffc77429f0db530164b10b12a4fa9
delasalle-sio-riquier-t <riquier.t@i12-12.sio.local>
2018-12-04 15:47:17 +0100

    Le projet

---
+++ src/ppe/testsunitaires/PasserelleServiceWebXMLTest.java
@@ -0,0 +1,75 @@
+package ppe.testsunitaires;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import ppe.classes.Outils;
+import ppe.classes.PasserelleServicesWebXML;
+import ppe.classes.Point;
+import ppe.classes.PointDeTrace;
+import ppe.classes.Trace;
+import ppe.classes.Utilisateur;
+
+public class PasserelleServiceWebXMLTest {
+	
+	@Test
+	public void testConnecter() {
+		fail("Not yet implemented");
+	}
+		
+	@Test
+	public void testCreerUnUtilisateur() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSupprimerUnUtilisateur() {
+		fail("Not yet implemented");	
+	}
+	
+	@Test
+	public void testChangerDeMdp() {
+		fail("Not yet implemented");
+	}	
+
+	@Test
+	public void testDemanderMdp() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDemanderUneAutorisation() {
+		fail("Not yet implemented");	
+	}	
+	
+	@Test
+	public void testRetirerUneAutorisation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEnvoyerPosition() throws ParseException {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDemarrerEnregistrementParcours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArreterEnregistrementParcours() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSupprimerUnUnParcours() {
+		fail("Not yet implemented");
+	}
+	
+} // fin du test
---
+++ src/ppe/testsvisuels/TestPasserelleServicesWebXML.java
@@ -0,0 +1,35 @@
+package ppe.testsvisuels;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import ppe.classes.Outils;
+import ppe.classes.PasserelleServicesWebXML;
+import ppe.classes.PointDeTrace;
+import ppe.classes.Trace;
+import ppe.classes.Utilisateur;
+
+public class TestPasserelleServicesWebXML {
+
+	public static void main(String[] args) throws ParseException {
+		
+		String msg;
+	
+		// test visuel de la méthode getTousLesUtilisateurs
+
+
+		// test visuel de la méthode getLesUtilisateursQueJautorise
+
+
+		// test visuel de la méthode getLesUtilisateursQuiMautorisent
+
+		
+		// test visuel de la méthode getLesParcoursDunUtilisateur
+
+		
+		// test visuel de la méthode getUnParcoursEtSesPoints
+
+	
+	} // fin Main
+} // fin class
